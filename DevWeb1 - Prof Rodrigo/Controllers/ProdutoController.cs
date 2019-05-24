a2e47f38754f05de00a6d62544d4106d2e83ccb7
Luiz Paulo Pereira <luizpaulopereiraf@gmail.com>
2019-05-24 10:19:17 -0400

    Criando ProdutoController

---
+++ DevWeb1 - Prof Rodrigo/Controllers/ProdutoController.cs
@@ -0,0 +1,56 @@
+﻿using DevWeb1___Prof_Rodrigo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DevWeb1___Prof_Rodrigo.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        // GET: Produto
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        public ActionResult Details()
+        {
+            return View();
+        }
+
+        public ActionResult Update()
+        {
+            return View();
+        }
+
+        public ActionResult Delete()
+        {
+            return View();
+        }
+
+
+        // POST: Produto
+        [HttpPost]
+        public ActionResult Create(Produto produto)
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Update(int id)
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+    }
+}
